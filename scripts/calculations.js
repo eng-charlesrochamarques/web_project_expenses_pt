7ba7181121ace0f03b0ae0ea6f1711e1a82a9a8a
Charles Marques <charlesrochamarques@gmail.com>
2025-11-04 16:45:24 -0300

    ADD Variables

---
+++ scripts/calculations.js
@@ -0,0 +1,11 @@
+let budgetValue;
+let totalExpensesValue;
+let expenseEntries = [
+  ["groceries", 33],
+  ["restaurants", 50],
+  ["transport", 12],
+  ["home", 70],
+  ["subscriptions", 14],
+  ["groceries", 28],
+  ["subscriptions", 12],
+];
